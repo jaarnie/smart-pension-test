3dd5c9caf3e6486d3809bdbc50df0f253992c601
John Arnold <johnadamarnold@gmail.com>
2020-08-07 13:54:25 +0100

    add 'returns correct data' test

--- spec/lib/parser_spec.rb
+++ spec/lib/parser_spec.rb
@@ -23,6 +23,18 @@ RSpec.describe Parser do
       it 'returns the log data as a hash' do
         expect(data).to be_a(Hash)
       end
+
+      it 'returns the correct data' do
+        expected_data = { '/help_page/1' =>
+                          ['126.318.035.038', '929.398.951.889', '722.247.931.582', '646.865.545.408'],
+                          '/contact' => ['184.123.665.067'],
+                          '/home' => ['184.123.665.067', '235.313.352.950'],
+                          '/about/2' => ['444.701.448.104'],
+                          '/index' => ['444.701.448.104'],
+                          '/about' => ['061.945.150.735'] }
+
+        expect(data).to eq(expected_data)
+      end
     end
   end
 end
