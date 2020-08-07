673eba8f66fa0d55063f128a758eff7e7fdd0701
John Arnold <johnadamarnold@gmail.com>
2020-08-07 14:04:53 +0100

    begin test and empty class with method names

---
+++ spec/lib/reader_spec.rb
@@ -0,0 +1,23 @@
+require_relative '../spec_helper'
+require_relative '../../lib/reader'
+
+RSpec.describe Reader do
+
+  context 'when calling most_views' do
+    it 'returns hash keys ordered by value count' do
+
+    end
+  end
+
+  context 'when calling unique_views' do
+    it 'returns hash keys ordered by unique values' do
+
+    end
+  end
+
+  context 'when creating a terminal table' do
+    it 'creates and displays correctly' do
+
+    end
+  end
+end
