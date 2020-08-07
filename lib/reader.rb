293d2f8dba9df05af746215ae6faa6651b9d33de
John Arnold <johnadamarnold@gmail.com>
2020-08-07 14:43:07 +0100

    begin reader class logic

--- lib/reader.rb
+++ lib/reader.rb
@@ -1,14 +1,26 @@
-class Reader
-  def initialize
+require 'terminal-table'
+require_relative 'parser'
 
+class Reader
+  def initialize(path)
+    @logs = Parser.new.parse_file(path)
   end
 
   def most_views
+    hash = {}
 
+    @logs.each do |k, v|
+      hash[k] = v.count
+    end
+
+    hash.sort_by(&:last).reverse.to_h
   end
 
   def unique_views
+    hash = Hash.new(0)
+    @logs.each { |k, v| hash[k] = v.uniq.count }
 
+    hash
   end
 
   def create_table
